be1666b7fbfc7a7a9e9f8ef75e27ba3e55427b0e
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2021-12-23 11:00:09 +0700

    exception

--- src/bai15/Main.java
+++ src/bai15/Main.java
@@ -1,7 +1,40 @@
 package bai15;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+//        Test.testException2();
+//        try {
+//            Test.testException();
+//        } catch (IOException e) {
+//            System.err.println(e.getMessage());
+//        }
+        try {
+            File file = new File("text.txt");
+            FileReader fileReader = new FileReader(file);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
+//        String a = "abc";
+//        int b = Integer.parseInt(a);
+//        System.out.println(b);
+    }
+
+    public static class Test {
+        static void testException() throws IOException {
+            throw new IOException("Lỗi để xử lý");
+        }
+
+        static void testException2() {
+            try {
+                throw new IOException("Lỗi đã xử lý");
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
